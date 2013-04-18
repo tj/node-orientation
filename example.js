16234f242efa18e330ae581ee3b26d0ca065f957
TJ Holowaychuk <tj@vision-media.ca>
2013-04-18 12:05:23 -0700

    Initial commit

---
+++ example.js
@@ -0,0 +1,7 @@
+
+var orientation = require('./');
+
+orientation('example.jpg', function(err, n){
+  if (err) throw err;
+  console.log(n);
+});
---
+++ index.js
@@ -0,0 +1,21 @@
+
+/**
+ * Module dependencies.
+ */
+
+var exec = require('child_process').exec;
+
+module.exports = function(file, fn){
+  var cmd = 'exiftool -Orientation -n ' + file;
+  exec(cmd, function(err, stdout){
+    if (err) return fn(err);
+
+    try {
+      var n = parseInt(stdout.split(':')[1], 10);
+    } catch (err) {
+      return fn(err);
+    }
+
+    fn(null, n);
+  });
+};
